f659170357530ea2dd36366128f8fdf81ae436d5
Daniel Bonofiglio <dev@dan.uy>
2026-07-07 15:38:27 -0300

    nix: export pi package in flake

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "PI agent harness";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+
+    pi = {
+      url = "github:earendil-works/pi";
+      flake = false;
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      pi,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pkgVersion = (builtins.fromJSON (builtins.readFile "${pi}/packages/agent/package.json")).version;
+
+        systemMapping = {
+          aarch64-darwin = "darwin-arm64";
+          aarch64-linux = "linux-arm64";
+          x86_64-darwin = "darwin-x64";
+          x86_64-linux = "linux-x64";
+        };
+        releasePlatform = systemMapping.${system};
+        releaseHashes = import ./pi-release-hashes.nix;
+
+        piCmd = pkgs.stdenv.mkDerivation (finalAttrs: {
+          pname = "pi";
+          version = pkgVersion;
+
+          src = fetchTarball {
+            url = "https://github.com/earendil-works/pi/releases/download/v${pkgVersion}/pi-${releasePlatform}.tar.gz";
+            name = "pi-coding-agent-${pkgVersion}";
+            sha256 = releaseHashes.${releasePlatform};
+          };
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir "$out"
+            cp -r "$src" "$out/bin"
+
+            runHook postInstall
+          '';
+        });
+      in
+      {
+        packages.pi = piCmd;
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            piCmd
+            nodejs
+          ];
+        };
+      }
+    );
+}
---
+++ pi-release-hashes.nix
@@ -0,0 +1,6 @@
+{
+  darwin-arm64 = "0k4z0vy5myk4r7za8nm69ggq8sds880q6mgp1zchx707fplbwxng";
+  darwin-x64 = "0mmchra2n9i33bc7fwx0k77zkjlg98b0rhgslqla88851ppfy21g";
+  linux-arm64 = "1rbqr9hpb8kf4xmqxmi2cw7w8s626bcf818wj883g67kiplxl27l";
+  linux-x64 = "146vnazrhgxkdfnbnh2pa0w6f46yg37m26q1vwsypd1dxprf57vy";
+}
